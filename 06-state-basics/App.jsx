dc69ee3d7cc32531568c662655f1f7980003ad16
Nicolás M. Pardo <nicolasm@nikodermus.media>
2020-05-29 15:42:05 -0500

    feat(06-state-basics): use a TV to show state

---
+++ 06-state-basics/App.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import './style.css';
+
+const show = '/show.png';
+const betty = '/betty.png';
+const midnight = '/midnight-gospel.png';
+const good = '/the-good-place.png';
+const twilight = '/twilight-zone.png';
+
+// Creamos una clase usando como base React.Componente
+// para tener todo el uso
+class App extends Component {
+    constructor(props) {
+        // Enviamos info a super, es decir Component
+        // para que pueda construir un React Component
+        super(props);
+
+        // Inicializamos el estado en el constructor
+        this.state = { on: '' };
+
+        // Conectamos los metodos para que apunten
+        // siempre a nuestro componente
+        this.turnTvOn = this.turnTvOn.bind(this);
+    }
+
+    // Creamos un metodo en nuestra clase
+    turnTvOn() {
+        // El estado no lo debes modificar directamente
+        // Usamos el metodo de los componentes "setState"
+        this.setState({ on: show });
+        // Le enviamos un objeto, con las propiedades que
+        // queremos reemplazar
+    }
+
+    // Aquí es donde tendremos nuestro JSX
+    // este metodo ya esta pre-establecido
+    // en lo qué es un componente
+    render() {
+        // Debemos hacer return del JSX
+        return (
+            <main>
+                <figure>
+                    <img className="show" src={this.state.on} alt="" />
+                    <img className="tv" src="/tv.png" alt="" />
+                </figure>
+
+                <button type="button" onClick={this.turnTvOn}>
+                    Prende la TV
+                </button>
+            </main>
+        );
+    }
+}
+
+export default App;
---
+++ 06-state-basics/README.md
@@ -0,0 +1,10 @@
+# State Basics
+
+-   ¿Por qué usamos `bind` en los metodos? [EN](https://www.freecodecamp.org/news/this-is-why-we-need-to-bind-event-handlers-in-class-components-in-react-f7ea1a6f93eb/) | [ES](https://medium.com/@ger86/por-qu%C3%A9-es-necesario-usar-bind-en-los-event-listener-de-los-components-d2f3a5944f4c)
+
+## Actividad
+
+Ya podemos apagar y prender la tele, pero solo nos muestra un canal. ¡ABURRIDO!. Ahora puedes usuar tu conocimiento para hacer alguno (o ambos) de estos retos. Cuando des click en la TV:
+
+-   Muestre al azar un canal nuevo.
+-   Haz una lista y que vaya de uno en uno, si llega al ultimo, que vuelva a comenzar desde el primero.
