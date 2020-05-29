8539355e2ef9a9556a5d7632b43b9dfee64796c7
Nicolás M. Pardo <nicolasm@nikodermus.media>
2020-05-28 23:25:51 -0500

    feat(04): add errors for JSX

--- 04-react-rules/App.jsx
+++ 04-react-rules/App.jsx
@@ -5,25 +5,23 @@ const App = () => {
     const date = new Date().getFullYear();
 
     return (
-        <>
             <main>
                 <figure>
                     <h2>HTML</h2>
-                    <img src="/doge.png" alt="doge" />
+                    <img src="../public/doge.png" alt="doge" >
                     <figcaption>
                         Hay 345 errores pero voy a renderizar lo que entiendo
                     </figcaption>
                 </figure>
                 <figure>
                     <h2>JSX</h2>
-                    <img src="/cheems.png" alt="cheems" className="small" />
+                    <img src="../public/cheems.png" alt="cheems" class="small" >
                     <figcaption>
                         Mamá, falta un slash y no muestro nada
                     </figcaption>
                 </figure>
             </main>
-            <footer>deBakatas memes {date} ©</footer>
-        </>
+            <footer>deBakatas memes date ©</footer>
     );
 };
 
