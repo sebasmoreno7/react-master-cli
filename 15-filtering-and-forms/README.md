03bb6465680e4be4df91abc53652e76bb5db495d
Nicolás M. Pardo <nicolasm@nikodermus.media>
2020-06-25 17:02:41 -0500

    feat(15-forms-and-filters): create project + readme

---
+++ 15-filtering-and-forms/README.md
@@ -0,0 +1,13 @@
+# Filtering and Forms
+
+> [Code Sandbox](https://codesandbox.io/s/15-filtering-and-forms-q3yo9)
+
+Tomemos una información compleja (Un array de objetos), y usemos un formulario para filtrar la diferente información de nuestros personajes.
+
+-   Buscar por parte de su nombre.
+-   Si es hombre o mujer
+-   Si es villano o heroe
+-   Edad, de 1 a [Infinito](https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Infinity)
+-   En donde aparece, TV, videoJuegos, comics o películas
+
+Estos filtros son **acumulativos**, es decir, que si tenemos activado el de mujeres y media = comic, solo me debe mostrar los resultados de personajes mujeres que aparezcan en comics.
---
+++ 15-filtering-and-forms/components/Filters.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const Filters = () => (
+    <form>
+        <div>
+            <label htmlFor="name">Search by name</label>
+            <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="e.j. Wonder Woman"
+            />
+            <fieldset>
+                <legend>Gender</legend>
+                <input type="radio" name="gender" value="male" id="male" />
+                <label htmlFor="male">Male</label>
+                <input type="radio" name="gender" value="female" id="female" />
+                <label htmlFor="female">Female</label>
+            </fieldset>
+            <fieldset>
+                <legend>Type</legend>
+                <input type="radio" name="type" value="villain" id="villain" />
+                <label htmlFor="villain">Villain</label>
+                <input type="radio" name="type" value="hero" id="hero" />
+                <label htmlFor="hero">Hero</label>
+            </fieldset>
+            <fieldset>
+                <legend>
+                    <label htmlFor="age">Age: 25</label>
+                </legend>
+                <input
+                    type="range"
+                    id="age"
+                    min="1"
+                    max="100"
+                    step="1"
+                    defaultValue=""
+                />
+            </fieldset>
+            <fieldset>
+                <legend>
+                    <label htmlFor="media">Media</label>
+                </legend>
+                <div className="select">
+                    <select name="media" id="media">
+                        <option value="" disabled selected>
+                            Select a media
+                        </option>
+                        <option value="tv">TV</option>
+                        <option value="movie">Movie</option>
+                        <option value="comic">Comic</option>
+                        <option value="Videogame">Videogame</option>
+                    </select>
+                </div>
+            </fieldset>
+        </div>
+    </form>
+);
+
+export default Filters;
